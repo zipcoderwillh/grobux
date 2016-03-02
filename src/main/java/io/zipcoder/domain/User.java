e03837e69d3bd4c08dd6c5ba87d488d82f48e3b3
jpbenge <jpbenge@gmail.com>
2016-03-02 16:31:14 -0500

    basic get and post for users

---
+++ src/main/java/io/zipcoder/domain/User.java
@@ -0,0 +1,43 @@
+package io.zipcoder.domain;
+
+import org.springframework.context.annotation.Primary;
+
+import javax.persistence.*;
+/**
+ * Created by johnb on 3/2/16.
+ */
+
+@Entity
+public class User {
+    @Id
+    @Column(name="USERNAME", unique=true)
+    private String userName;
+    @Column(name="PASSWORD")
+    private String password;
+    @Column(name="CAPITOL_ONE_ID")
+    private String capitolOneID;
+    @Column(name="FIRSTNAME")
+    private String firstName;
+    @Column(name="LASTNAME")
+    private String lastName;
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getCapitolOneID() {
+        return capitolOneID;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+}
