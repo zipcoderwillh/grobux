7e9f4ceefd4c945fcf5423db6ec5eb68f6bba838
jpbenge <jpbenge@gmail.com>
2016-03-02 12:03:18 -0500

    renamed default package

--- src/main/java/io/zipcoder/GrobuxApplication.java
+++ src/main/java/io/zipcoder/GrobuxApplication.java
@@ -1,4 +1,4 @@
-package com.example;
+package io.zipcoder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/io/zipcoder/GrobuxApplicationTests.java
+++ src/test/java/io/zipcoder/GrobuxApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example;
+package io.zipcoder;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
