a9cbd0a0606d52e19f388a0c7613c4263a686c54
jpbenge <jpbenge@gmail.com>
2016-03-09 14:34:41 -0500

    modified application.properties

--- src/main/java/io/zipcoder/repository/UserRepository.java
+++ src/main/java/io/zipcoder/repository/UserRepository.java
@@ -7,6 +7,7 @@ import org.springframework.transaction.annotation.Transactional;
 /**
  * Created by johnb on 3/2/16.
  */
+
 @Transactional
 public interface UserRepository extends CrudRepository<User, String>{
 }
