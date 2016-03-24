3de6d57422019a54ed969b1b1744524782ccdd01
willhorton <willhorton@Zips-MacBook-Pro-4.local>
2016-03-24 11:17:09 -0400

    Added update and delete methods to TripController

--- src/main/java/io/zipcoder/controller/TripController.java
+++ src/main/java/io/zipcoder/controller/TripController.java
@@ -1,15 +1,13 @@
 package io.zipcoder.controller;
 
 import javax.inject.Inject;
+import javax.xml.ws.Response;
 
 import io.zipcoder.repository.UserRepository;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import io.zipcoder.repository.TripRepository;
 import io.zipcoder.domain.Trip;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
@@ -45,4 +43,16 @@ public class TripController {
 
     }
 
+    @RequestMapping(value="/trips/{tripId}", method=RequestMethod.PUT)
+    public ResponseEntity<?> updateTrip(@RequestBody Trip trip, @PathVariable Long tripId) {
+        Trip t = tripRepository.save(trip);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    @RequestMapping(value="trips/{tripId}", method=RequestMethod.DELETE)
+    public ResponseEntity<?> deleteTrip(@PathVariable Long tripId) {
+        tripRepository.delete(tripId);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
+    
 }
